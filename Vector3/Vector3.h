51af9a59991234f00551fe7295c317fec6883239
Gen346 <kelzreez@gmail.com>
2025-09-03 20:32:15 +0300

    1. Constructors added;
    2. Operators oveloading added (+, -, *, ==);
    
    3. Normalize function added;
    
    4. length() added;

--- Vector3/Vector3.h
+++ Vector3/Vector3.h
@@ -2,6 +2,66 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+#include <cmath>
+#include <iostream>
 
+struct Vector3
+{
+	Vector3() : x(0), y(0), z(0) {};
+	Vector3(float x, float y, float z) : x(x), y(y), z(z) {};
+
+	float x;
+	float y;
+	float z;
+
+	inline Vector3 operator+(const Vector3& rhs) const
+	{
+		return { x + rhs.x, y + rhs.y, z + rhs.z };
+	}
+
+	inline Vector3 operator-(const Vector3& rhs) const
+	{
+		return { x - rhs.x, y - rhs.y, z - rhs.z };
+	}
+
+	inline Vector3 operator*(float scalar) const
+	{
+		return { x * scalar, y * scalar, z * scalar };
+	}
+
+	friend inline Vector3 operator*(float scalar, const Vector3& rhs)
+	{
+		return { scalar * rhs.x, scalar * rhs.y, scalar * rhs.z };
+	}
+
+	inline bool operator==(const Vector3& rhs) const
+	{
+		const float EPS = 1e-6f;
+
+		return std::fabs(x - rhs.x) < EPS && std::fabs(y - rhs.y) < EPS && std::fabs(z - rhs.z) < EPS;
+	}
+
+	friend inline std::ostream& operator<<(std::ostream& os, const Vector3& rhs)
+	{
+		os << "(" << rhs.x << ", " << rhs.y << ", " << rhs.z << ")";
+		return os;
+	}
+
+	void normalize()
+	{
+		float length = sqrtf(x * x + y * y + z * z);
+
+		if (length != 0)
+		{
+			x / length;
+			y / length;
+			z / length;
+		}
+	}
+	float length() const
+	{
+		return sqrtf(x * x + y * y + z * z);
+	}
+};
 
 #endif
